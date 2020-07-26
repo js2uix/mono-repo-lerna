16d8f723499555646c62ee73170ebb7d0318005d
js2uix <deshineplus@naver.com>
2020-07-26 12:31:30 +0900

    myPackage

---
+++ packages/my-package/__tests__/my-package.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const myPackage = require('..');
+
+describe('my-package', () => {
+    it('needs tests');
+});
---
+++ packages/my-package/lib/my-package.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = myPackage;
+
+function myPackage() {
+    // TODO
+}
